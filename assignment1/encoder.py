55228e0d96f1661c42a54c298e7b80f42ecc95eb
Xiaoyan Liu <xliu730@gatech.edu>
2020-09-18 22:08:36 -0700

    add data

---
+++ assignment1/encoder.py
@@ -0,0 +1,80 @@
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+def feature_encode_categorical(df, categorical_columns):
+    
+    encoders = {}
+    for column in categorical_columns:
+        encoders[column] = LabelEncoder()
+        print (column)
+        df[column] = encoders[column].fit_transform(df[column])
+    return df 
+
+
+def onehot_encoder(df, categorical_columns):
+    #feature encoding 
+    X = feature_encode_categorical(df, categorical_columns)[categorical_columns]
+    print (X)
+    ohe = OneHotEncoder(sparse=False)
+    X_ohe = ohe.fit_transform(X)
+    
+    columns = ohe.get_feature_names(categorical_columns)
+    
+    X_cat = pd.DataFrame(X_ohe, columns = columns)
+    data = pd.concat([df, X_cat], axis=1)
+
+    return data
+
+def get_dummies(df, categorical_columns):
+    
+    return pd.get_dummies(df, columns = categorical_columns)
+
+
+def target_encoder(df, categorical_columns, target, smooth=False, m=None):
+
+    y_onehot = get_dummies(df[target], [target])
+    
+    
+    class_names = y_onehot.columns.tolist()
+    
+    df = pd.concat([df, y_onehot], axis=1)
+    
+
+    for column in categorical_columns:
+        for class_ in class_names:
+            mean_value = _target_encoder_by_column(df, column, class_)
+            df[column + '_' + str(class_)] = df[column].map(mean_value)
+            
+    return df.drop(categorical_columns + class_names, axis=1)
+
+def _target_encoder_by_column(df, column, target, smooth=False, m=None):
+
+    # modified on top of https://maxhalford.github.io/blog/target-encoding/
+
+    if smooth:
+        # global_mean 
+        global_mean = df[target].mean()
+
+        
+        agg =  df.groupby(column)[target].agg(['count', 'mean'])
+
+        mean_val = agg['mean']
+        counts = agg['count']
+
+        smooth = (counts * mean_val + m * counts * global_mean) / (counts + m * counts)
+
+        return smooth
+
+    else:
+        
+        mean_val = df.groupby(column)[target].mean()
+        return mean_val
+
+
+
+
+
+
+
+    
+
+
---
+++ assignment1/model_train_eval.py
@@ -0,0 +1,249 @@
+
+import os
+import time
+import warnings
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSearchCV
+
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+import time 
+
+
+
+
+def rebalance_data(df, target, strategy="SMOTE"):
+    
+    X = df.loc[:, df.columns != target]
+    y = df.loc[:, df.columns == target]
+
+    if len(set(y[target])) == 1:
+
+        print ('Only one class {} of label is generated'.format(list(set(y)))[0])
+
+        return df 
+    else:
+
+        if strategy=='SMOTE':
+            sm = SMOTE(sampling_strategy='auto', k_neighbors=1, random_state=42)
+            X_smote, y_smote = sm.fit_resample(X, y)
+            X_smote[target] = y_smote
+            df = X_smote
+            #  print ('after smote rebalanced, data shape is {}'.format(df.target.value_counts()))
+        if strategy=='Subsampling':
+
+            #rus = RandomUnderSampler(sampling_strategy=0.5)
+            rus = RandomUnderSampler()
+            x_rus, y_rus = rus.fit_sample(X,y)
+            x_rus[target] = y_rus
+            df = x_rus
+    return df
+
+def fit_model(model, model_args, X_train, y_train):
+    classifier = model(**model_args)
+    classifier.fit(X_train, y_train)
+
+    return classifier 
+
+
+def model_performance(model, X, y, average='macro'):
+
+    predictions = model.predict(X)
+    probs = model.predict_proba(X)
+    f1= f1_score(y, predictions, average=average)
+    precision = precision_score(y, predictions, average=average)
+    recall = recall_score(y, predictions, average=average)
+    accuracy = accuracy_score(y, predictions)
+    roc_auc = roc_auc_score(y, probs, multi_class='ovr', average=average)
+    report = classification_report(y, predictions)
+
+    return f1, precision, recall, accuracy, roc_auc, report
+
+
+from sklearn.preprocessing import StandardScaler 
+
+
+
+def normalizer(X_train, X_test, scaler_fn=StandardScaler ):
+
+    scaler = scaler_fn()
+    X_train_trans = scaler.fit_trainsform(X_train)
+    X_test_trans = scaler.transform(X_test)
+
+
+    return X_train_trans, X_test_trans 
+
+
+
+
+def cm_cv(m, n, X, y, score):
+    cm = model_selection.cross_val_score(m, X, y, scoring=score, cv=n)
+
+
+def hyperparameter_sweep(model,  X, y, k, score_fn, search_args, strategy='gridsearch'):
+
+    if strategy == 'gridsearch':
+        
+        clf = GridSearchCV(model(), cv=k,  scoring = score_fn , param_grid=search_args)
+
+    elif strategy == 'randomsearch':
+        clf = RandomizedSearchCV(model(), cv=k, scoring= score_fn, param_distributions=search_args )
+
+    search = clf.fit(X, y)
+    results = search.cv_results_
+    best_model = search.best_estimator_
+    best_param = search.best_params_
+    best_score = search.best_score_
+
+    return {'results': results,
+            'best_model': best_model,
+            'best_param': best_param,
+            'best_score': best_score}
+
+
+
+def train_eval(df, model, target, config, k, balancer=False, hypertune=False, test_size = 0.3, model_args=None):
+    
+    if balancer:
+    
+        df = rebalance_data(df, target)
+        
+    
+    
+    X = df.loc[:, df.columns != target]
+    y = df.loc[:, df.columns == target]
+    
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size, random_state = seed)
+    
+    is_normalizer= config[model]['normalizer']
+    
+    if is_normalizer:
+        print ('normalize data first!')
+        X_train, X_test = normalizer(X_train, X_test)
+        
+    
+    print (X_train.shape)
+
+    if hypertune:
+        print (config[model])
+        search_args = config[model]['param_values']
+        model_fn =  config[model]['model']
+        score_fn = config[model]['score_fn']
+        start = time.time()
+        results_dict = hyperparameter_sweep(model_fn, X_train, y_train, k, score_fn, search_args, strategy='gridsearch')
+        classifier = results_dict['best_model']
+        end = time.time()
+        elapsed = end - start
+        info = results_dict['results']
+        
+    else:
+        model_fn = config[model]['model']
+        print (model_fn)
+        model_args = config[model]['default_model_args']
+        start = time.time()
+        print ('start')
+        classifier = fit_model(model_fn, model_args, X_train, y_train)
+        print ('end')
+        end = time.time()
+        elapsed = end - start
+        info = None
+        
+    
+        
+    print ('fit model finished')   
+        
+    f1, precision, recall, accuracy, roc_auc, report = model_performance(classifier, X_test, y_test, average='macro')
+        
+    return {'classifier': classifier,
+            'train_time_elapsed': elapsed,
+        'f1': f1, 
+            'precision': precision,
+           'recall': recall,
+           'accuracy': accuracy,
+           'roc_auc': roc_auc, 
+           'classification_report': report,
+           'info': info }
+
+
+
+    
+
+
+
+
+    
+
+
+
+
+    
+model_config = {
+    'DecisionTreeClassifier': 
+        {
+            'model': DecisionTreeClassifier,
+            'normalizer': False,
+            'param_values':  
+                {
+                    "criterion": ['gini', 'entropy'],
+                    "max_depth": [5, 10, 20,50,100],
+                    "min_samples_leaf": [ 5, 10,20]
+                },              
+            'score_fn': 'f1_macro',
+            'default_model_args': 
+                {
+                    'criterion': 'entropy',
+                    'max_depth': 20,
+                    'min_samples_leaf': 5
+                }
+        },
+    'GradientBoostingClassifier':
+        {
+            'model': GradientBoostingClassifier,
+             'normalizer': False,
+            'param_values':
+                {
+                    "learning_rate": [0.05, 0.1, 0.5],
+                    "n_estimators": [50, 100, 200],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [5, 10, 30, 50]
+                },
+            'score_fn': 'f1_macro',
+            'default_model_args':
+                {
+                    "n_estimators": 200,
+                    "min_samples_leaf": 50
+                }
+        },
+
+    'SVC':
+        {
+            'model': SVC,
+            'normalizer': True,
+            'param_values':
+                {
+                    "kernel" : ['linear', 'rbf', 'poly']
+                    
+                },
+
+            'score_fn': 'f1_macro',
+            'default_model_args':
+                {
+                    "kernel": 'linear',
+                    "C": 1
+                }
+
+        }
+    
+    
+    
+    
+    
+    }
+
